77763a24e0cf09e512e28d15341a436aac145451
Vijay <vijayganganapalli@gmail.com>
2018-11-13 08:54:51 +0530

    adding a click listener

--- app/src/main/java/com/arvi/aboutme/MainActivity.kt
+++ app/src/main/java/com/arvi/aboutme/MainActivity.kt
@@ -1,12 +1,40 @@
 package com.arvi.aboutme
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Context
 import android.os.Bundle
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var buttonDone: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        buttonDone = findViewById(R.id.button_done)
+
+        buttonDone.setOnClickListener {
+            addNickname(it)
+        }
+    }
+
+    private fun addNickname(view: View) {
+        val editTextNickname = findViewById<EditText>(R.id.edit_nickname)
+        val textViewNickname = findViewById<TextView>(R.id.text_nickname)
+
+        textViewNickname.text = editTextNickname.text
+        editTextNickname.visibility = View.GONE
+        view.visibility = View.GONE
+        textViewNickname.visibility = View.VISIBLE
+
+        //Hide the keyboard
+        val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.hideSoftInputFromWindow(view.windowToken, 0)
     }
 }
